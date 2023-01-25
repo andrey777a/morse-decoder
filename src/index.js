d70e5eb820dad273ec948714ec704f5869a87a6f
Andrey <andriiano@yandex.ru>
2023-01-25 22:52:12 +0300

    add: done

--- src/index.js
+++ src/index.js
@@ -38,7 +38,28 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let arr = [];
+    for (let i = 0; i < expr.length/10; i++) {
+        arr.push(expr.slice(i*10, (i+1)*10));
+    }
+    arr = arr.map((item) => {
+        let str = '';
+        for (let i = 0; i < item.length/2; i++) {
+            if (item.slice(i*2, (i+1)*2) === '10') str += '.'
+            else if (item.slice(i*2, (i+1)*2) === '11') str += '-'
+            else if (item.slice(i*2, (i+1)*2) === '**') {
+                str += ' ';
+                break;
+            }
+        }
+        return str;
+    })
+    let result = '';
+    arr.forEach((item) => {
+        if (item === ' ') result += ' ';
+        else result += MORSE_TABLE[item];
+    })
+    return result;
 }
 
 module.exports = {
